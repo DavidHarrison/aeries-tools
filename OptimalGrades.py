adfb6e0be7ed715e83d5dc52560f4b154148c9e5
David Harrison <djrh777@gmail.com>
2013-11-26 15:24:58 -0800

    convert to multiline comments

--- OptimalGrades.py
+++ OptimalGrades.py
@@ -36,22 +36,30 @@ CATEGORY_PERCENT = 'percent'
 ### name of the category for gradebook totals
 TOTAL_CATEGORY = 'Total'
 
-# optimal way to get target_percent with assignments, given gradebook_categories
-#   target_percent: float (probably 0-100)
-#   assignments: list of assignment dictionaries (from aeries-api)
-#   gradebook_categories: list of category dictionaries from the gradebook
-#       (with or without weighting) (from aeries-api)
+'''
+optimal way to get target_percent with assignments, given gradebook_categories
+    target_percent: float (probably 0-100)
+    assignments: list of assignment dictionaries (from aeries-api)
+    gradebook_categories: list of category dictionaries from the gradebook
+        (with or without weighting) (from aeries-api)
+'''
 def optimalGrades(target_percent, assignments, gradebook_categories):
-#Minimizing the cost of the set of percentages
+    '''
+    Minimizing the cost of the set of percentages
+    '''
     '''
     determine a reasonable starting grade set (eg target grade for all
         but last assignment (for which the grade will be calculated))
     '''
-    # set start_percent to the abitrary (and hopefully close-ish) target_percent
+    '''
+    set start_percent to the abitrary (and hopefully close-ish) target_percent
+    '''
     start_percent = target_percent
     assignment_scores = []
     i = 0
-    # for each assignment (excepting the last), give it a starting score of start_percent
+    '''
+    for each assignment (excepting the last), give it a starting score of start_percent
+    '''
     while i < len(assignments) - 1:
         assignment_scores.append(str(start_percent))
         i += 1
@@ -74,10 +82,15 @@ def optimalGrades(target_percent, assignments, gradebook_categories):
         i += 1
     return optimal_scores_dict
 
-def getNeededPercent(target_percent, assignment, gradebook_categories, assignments=default, assignment_scores=default):
+def getNeededPercent(target_percent, assignment, gradebook_categories,
+                        assignments=default, assignment_scores=default):
     min_percent = getMinPercent(assignments, gradebook_categories)
-    other_assignment_deltas = getOtherAssignmentDeltas(assignments, assignment_scores, gradebook_categories, assignment)
-    overall_assignment_weight = getAssignmentWeight(assignment, assignments, gradebook_categories)
+    other_assignment_deltas = getOtherAssignmentDeltas(assignments,
+                                                        assignment_scores,
+                                                        gradebook_categories,
+                                                        assignment)
+    overall_assignment_weight = getAssignmentWeight(assignment, assignments,
+                                                    gradebook_categories)
     overall_delta_needed = target_percent - min_percent - other_assignment_deltas
     score_neeeded = overall_delta_needed/overall_assignment_weight
 
@@ -86,8 +99,12 @@ def getMinPercent(assignments, gradebook_categories):
     for category in assignments:
         if category[CATEGORY_NAME] == TOTAL_CATEGORY:
             continue
-        category_weight = getCategoryWeight(category[CATEGORY_NAME], assignments, gradebook_categories)
-        min_category_percent = getMinCategoryPercent(assignment[CATEGORY_NAME], assignments, gradebook_categories)
+        category_weight = getCategoryWeight(category[CATEGORY_NAME],
+                                            assignments,
+                                            gradebook_categories)
+        min_category_percent = getMinCategoryPercent(assignment[CATEGORY_NAME],
+                                                        assignments,
+                                                        gradebook_categories)
         min_percent += category_weight * min_category_percent
     return min_percent
 
@@ -124,7 +141,9 @@ def isWeightingUsed(gradebook_categories):
     except:
         return False
 
-# return the category dictionary of name category_name from the list of categories
+'''
+return the category dictionary of name category_name from the list of categories
+'''
 def getCategory(category_name, gradebook_categories):
     for category in categories:
         if category[CATEGORY_NAME] == category_name:
@@ -145,7 +164,9 @@ def getWeightingUsed(assignments, gradebook_categories):
             category_weight = getGivenCategoryWeight(category)
             weightin_used += category_weight
 
-# return whether or not the category is active (has non-zero assignments in it)
+'''
+return whether or not the category is active (has non-zero assignments in it)
+'''
 def isCategoryActive(category):
     if category[CATEGORY_MAX_POINTS] == 0:
         return False
@@ -175,14 +196,16 @@ def getMinCategoryPercent(category_name, assignments, gradebook_categories):
                 recieved (but not max points) to give it a score of zero
             '''
             if isAssignmentGraded(assignment):
-                current_category_score_points -= float(ASSIGNMENT[ASSIGNMENT_SCORE_POINTS])
+                current_category_score_points -=
+                        float(ASSIGNMENT[ASSIGNMENT_SCORE_POINTS])
             '''
             if the assignment has not yet been graded, add its max points to the
                 categories max points and do not add any score points to add the
                 assignment with a score of zero
             '''
             else:
-                current_category_max_points += float(ASSIGNMENT[ASSIGNMENT_SCORE_POINTS])
+                current_category_max_points +=
+                        float(ASSIGNMENT[ASSIGNMENT_SCORE_POINTS])
         else:
             continue
     min_category_percent = category_score_points / category_max_points
@@ -191,36 +214,44 @@ def getMinCategoryPercent(category_name, assignments, gradebook_categories):
 def isAssignmentGraded(assignment):
     if assignment[GRADING_COMPLETE] == ASSIGNMENT_GRADING_COMPLETE_TRUE:
         return True
-    elif assignment[ASSIGNMENT_GRADING_COMPLETE] == ASSIGNMENT_GRADING_COMPLETE_FALSE:
+    elif assignment[ASSIGNMENT_GRADING_COMPLETE] ==
+            ASSIGNMENT_GRADING_COMPLETE_FALSE:
         return False
     else:
-        print "Undefined value for grading completion: " + str(assignment[ASSIGNMENT_GRADING_COMPLETE])
+        print "Undefined value for grading completion: " +
+                str(assignment[ASSIGNMENT_GRADING_COMPLETE])
         raise
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 '''
-return the change in the total grade made by all assignments other than assignment
-    (with scores assignment_scores, where each of the indices of in assignments and
-    assignment_scores align)
+return the change in the total grade made by all assignments other than
+    assignment (with scores assignment_scores, where each of the indices of in
+    assignments and assignment_scores align)
 '''
-def getOtherAssignmentDeltas(assignments, assignment_scores, gradebook_categories, exclude_assignment):
+def getOtherAssignmentDeltas(assignments, assignment_scores, gradebook_categories,
+        exclude_assignment):
     total_assignment_delta = 0
     i = 0
     for assignment in assignents:
         if assignment[ASSIGNMENT_NAME] == exclude_assignment[ASSIGNMENT_NAME]:
             continue
         else
-            assignment_delta = getAssignmentDelta(assignment, assignment_scores[i], gradebook_categories):
+            assignment_delta = getAssignmentDelta(assignment,
+                                                    assignment_scores[i],
+                                                    gradebook_categories):
             total_assignment_delta += assignment_delta
         i += 1
     return total_assignment_delta
 
 '''
-return the change made to the total grade by assignment (with score assignment_score)
+return the change made to the total grade by assignment
+    (with score assignment_score)
 '''
-def getAssignmentDelta(assignment, assignment_score, assignments, gradebook_categories):
-    overall_assignment_weight = getAssignmentWeight(assignment, assignments, gradebook_categories):
+def getAssignmentDelta(assignment, assignment_score, assignments,
+                        gradebook_categories):
+    overall_assignment_weight = getAssignmentWeight(assignment, assignments,
+                                                    gradebook_categories):
     assignment_delta = assignment_score * overall_assignment_weight
 
 '''
@@ -235,7 +266,8 @@ def getAssignmentWeight(assignment, assignments, gradebook_categories):
         total_points_max = getTotalPointsMax(assignments, gradebook_categories)
         assignment_weight_in_category = 1.0 / total_points_max
     else:
-        assignment_weight_in_category = getAssignmentWeightInCategory(assignments, category)
+        assignment_weight_in_category =
+                getAssignmentWeightInCategory(assignments, category)
     assignment_weight = assignment_weight_in_category * category_weight
 
 def getTotalPointsMax(assignments, gradebook_categories):
@@ -260,12 +292,8 @@ def getAssignmentWeightInCategory(assignments, category):
     assignment_weight_in_category = 1.0 / category_max
     return assignment_weight_in_category
 
-
-
-
-
-
 #===============================================================================
+
 def optimize(target_percent, assignments, current_position, gradebook_categories):
     blank_move = {}
     moves = getMoves(blank_move, current_position, OPTIMIZATION_DISTANCE)
@@ -277,7 +305,8 @@ def optimize(target_percent, assignments, current_position, gradebook_categories
     else:
         return optimize(target_percent, assignments, next_move, gradebook_categories)
 
-def getMoves(partial_move, current_position, remaining_vector_distance, target_percent, assignments, gradebook_categories))
+def getMoves(partial_move, current_position, remaining_vector_distance,
+                target_percent, assignments, gradebook_categories))
     try:
         if len(partial_move) == len(current_position) - 1:
             final_assignment = assignments[len(assignments)-1]
@@ -286,7 +315,9 @@ def getMoves(partial_move, current_position, remaining_vector_distance, target_p
                                                 assignments=assignments,
                                                 assignments_scores=partial_move)
             final_move = partial_move.append(needed_percent)
-            # return final_move as a list so that it can be added to a list
+            '''
+            return final_move as a list so that it can be added to a list
+            '''
             return [final_move]
     except:
         pass
@@ -295,20 +326,32 @@ def getMoves(partial_move, current_position, remaining_vector_distance, target_p
         new_moves = []
         i = 0
         '''
-        relative safe float comparison, making sure i does not exceed remaining_vector_distance
-            by more an increment or more
+        relative safe float comparison, making sure i does not exceed
+            remaining_vector_distance by more an increment or more
         '''
         while remaining_vector_distance - i > -1 * (0.5 * increment):
-            # current_position at the index of the next score to be added to partial_move
+            '''
+            current_position at the index of the next score to be added
+                to partial_move
+            '''
             current_score_value = current_position[len(partial_move)]
-            # the adjusted score
+            '''
+            the adjusted score
+            '''
             new_score_value = current_score_value + i
-            # copy partial_move and put in a distance for the current variable
+            '''
+            copy partial_move and put in a distance for the current variable
+            '''
             new_partial_move = list(partial_move).append(new_score_value)
-            # intentional use of += to put all moves on the same level
+            '''
+            intentional use of += to put all moves on the same level
+            '''
             new_distance_left = Math.sqrt(remaining_vector_distance**2 - i**2)
-            new_moves += getMoves(new_partial_move, current_position, new_distance_left)
-            # increment i so that it will loop through MOVES_PER_SCORE times
+            new_moves += getMoves(new_partial_move, current_position,
+                                    new_distance_left)
+            '''
+            increment i so that it will loop through MOVES_PER_SCORE times
+            '''
             i += increment
             return new_moves
 
@@ -320,12 +363,17 @@ def getMinCostMove(moves, assignments, gradebook_categories):
             min_cost_move = move
             min_cost_move_cost = move_cost
 
-# vector distance squared (no need to root) from the averages for the assignment categories
+'''
+vector distance squared (no need to root) from the averages for the
+    assignment categories
+'''
 def getCost(position, assignments, gradebook_categories):
     cost = 0
     for score in position:
         score_assignment = assignments[position.index(score)]
-        score_category_average = getAssignmentCategoryAverage(score_assignment, gradebook_categories)
+        score_category_average =
+                getAssignmentCategoryAverage(score_assignment,
+                                                gradebook_categories)
         cost += (score - score_category_average)**2
     return cost
 
